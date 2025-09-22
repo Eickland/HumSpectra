5b25a9cfd07d68a55f305273b151b5dd09182ba9
Kirill <mnbv21228@gmail.com>
2025-09-22 14:41:12 +0300

    0.0.121A

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="HumSpectra",
-    version="0.0.120",
+    version="0.0.121",
     author="Kirill",
     author_email="mnbv21228@mail.ru",
     description="Обработка спектров уф и флуоресценции",
