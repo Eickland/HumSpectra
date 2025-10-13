f7ee3b7123d65ab37ef4c517eb031e6479721d39
Kirill <mnbv21228@mail.ru>
2025-10-13 16:54:59 +0300

    fix Baikal subclass check up

--- HumSpectra/utilits.py
+++ HumSpectra/utilits.py
@@ -105,7 +105,7 @@ def extract_subclass_from_name(file_name: str) -> str:
         
         split_name = str_name.split(sep="-")
 
-        if "B" in split_name:
+        if "B" in split_name  or "В" in split_name:
             sample_subclass = "Baikal"
             
         else:
