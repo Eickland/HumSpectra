8374afcb32559ef6aa43ad9a2614decac2aa23e4
Kirill <mnbv21228@gmail.com>
2025-12-04 16:53:56 +0300

    fix extract_subclass_from_name

--- HumSpectra/utilits.py
+++ HumSpectra/utilits.py
@@ -151,15 +151,9 @@ def extract_subclass_from_name(file_name: str) -> str:
     
     elif "ADOM" == sample_class:
         
-        split_name = str_name.split(sep="-")
-
-        if len(split_name) == 2:
-                str_name = split_name[0][:1]+"-"+split_name[0][1:]
-
-                str_subclass = str_name.split(sep="-")[1]
-                sample_subclass = "Storage " + str(extract_and_combine_digits_re(str_subclass))
+        str_subclass = str_name.split(sep="-")[1]
+        sample_subclass = "Storage " + str(extract_and_combine_digits_re(str_subclass))
         
-
     elif "L" == str_name[0]:
         sample_subclass = "Lg"
 
